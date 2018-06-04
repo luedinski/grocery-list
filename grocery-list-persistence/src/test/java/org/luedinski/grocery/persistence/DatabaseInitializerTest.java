09d18389c218a49727a045118a296bf0a04a53f3
Lüder Giffey-Briesenick <Giffey@encoway.de>
2018-06-04 16:31:14 +0200

    test

---
+++ grocery-list-persistence/src/test/java/org/luedinski/grocery/persistence/DatabaseInitializerTest.java
@@ -0,0 +1,27 @@
+package org.luedinski.grocery.persistence;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.SQLException;
+
+import com.j256.ormlite.db.DatabaseType;
+import com.j256.ormlite.support.ConnectionSource;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.luedinski.grocery.persistence.model.User;
+
+public class DatabaseInitializerTest {
+
+    @Test
+    public void testInit() throws Exception{
+        ConnectionSource source = mock(ConnectionSource.class);
+        DatabaseType dbType = mock(DatabaseType.class);
+        when(source.getDatabaseType()).thenReturn(dbType);
+        when(dbType.extractDatabaseTableConfig(source, User.class)).thenThrow(new SQLException("test error"));
+        DatabaseInitializer subject = new DatabaseInitializer(source, User.class);
+        Assertions.assertThatExceptionOfType(IllegalStateException.class).isThrownBy(subject::init)
+                .withCauseExactlyInstanceOf(SQLException.class)
+                .withMessageContaining("test error");
+    }
+}
